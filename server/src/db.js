9bcfbe3f97116af6ac51b688f1ba853fe09f341c
NickJohnson06 <acejohnny06@gmail.com>
2025-07-22 23:08:15 -0400

    Updated .env and db.js to establish db connection

--- server/src/db.js
+++ server/src/db.js
@@ -1,1 +1,14 @@
 //Mongoose connection
+import mongoose from "mongoose";
+
+export default async function connectDB() {
+  const uri = process.env.MONGODB_URI;
+  if (!uri) throw new Error("MONGODB_URI missing");
+
+  const opts = {
+    serverApi: { version: '1', strict: true, deprecationErrors: true },
+  };
+
+  await mongoose.connect(uri, opts);
+  console.log("MongoDB connected");
+}

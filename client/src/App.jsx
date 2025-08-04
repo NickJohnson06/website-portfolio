98a50d1b243027a8d87a17012e0ff710881f60be
NickJohnson06 <acejohnny06@gmail.com>
2025-08-04 19:40:43 -0400

    Updated index.css and App.jsx files

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
+import Home from './pages/Home'
+import Projects from './pages/Projects'
+import Resume from './pages/Resume'
+import Contact from './pages/Contact'
+
+function App() {
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Layout>
+  )
+}
+
+export default App
